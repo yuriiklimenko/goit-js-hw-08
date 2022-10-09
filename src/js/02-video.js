e355d58a307de063e650d1a9fdaad4a51ef39b04
yuriiklimenko <109259174+yuriiklimenko@users.noreply.github.com>
2022-10-09 12:48:06 +0300

    fix videoplayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,20 +2,14 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
+const CURRENT__TIME = 'videoplayer-current-time';
 
 const player = new Player(iframe);
 
-const CURRENT__TIME = 'videoplayer-current-time';
+player.setCurrentTime(localStorage.getItem(CURRENT__TIME));
 
 const saveCurrentTime = data => {
   localStorage.setItem(CURRENT__TIME, data.seconds);
 };
 
-const onPlayVideo = () => {
-  player.setCurrentTime(localStorage.getItem(CURRENT__TIME));
-  player.off('play', onPlayVideo);
-};
-
 player.on('timeupdate', throttle(saveCurrentTime, 1000));
-
-player.on('play', onPlayVideo);
